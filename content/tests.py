5966fcd35a05d679b4090815109f2cb332afee3c
DHayes23 <D.Hayes23@hotmail.com>
2024-08-05 21:34:43 +0000

    Update test suite to reflect decoupled lessons/modules

--- content/tests.py
+++ content/tests.py
@@ -4,28 +4,25 @@ from .models import Path, Module, Lesson
 class ContentModelTest(TestCase):
     
     def test_path_creation(self):
-        # Test that a Path object can be created with the correct name and exists in the database
+        # Test that a path is correctly created
         path = Path.objects.create(name='Path 1', description='Test Path')
         self.assertEqual(path.name, 'Path 1', "Path should be created with the correct name.")
         self.assertEqual(Path.objects.count(), 1, "There should be one Path in the database.")
 
     def test_module_creation(self):
-        # Test that a Module object can be created with the correct name and exists in the database
+        # Test that a module is correctly created
         module = Module.objects.create(name='Module 1', description='Test Module')
         self.assertEqual(module.name, 'Module 1', "Module should be created with the correct name.")
         self.assertEqual(Module.objects.count(), 1, "There should be one Module in the database.")
 
     def test_lesson_creation(self):
-        # Test that a Lesson object can be created and associated with a Module
-        module = Module.objects.create(name='Module 1')
+        # Test that a lesson is correctly created
         lesson = Lesson.objects.create(name='Lesson 1', lesson_type='text')
-        module.lessons.add(lesson)
         self.assertEqual(lesson.name, 'Lesson 1', "Lesson should be created with the correct name.")
         self.assertEqual(Lesson.objects.count(), 1, "There should be one Lesson in the database.")
-        self.assertIn(lesson, module.lessons.all(), "Lesson should be correctly linked to its Module.")
 
     def test_path_module_relationship(self):
-        # Test that Modules can be associated with a Path and retrieved correctly
+        # Test the relationship between paths and modules
         path = Path.objects.create(name='Path 1')
         module1 = Module.objects.create(name='Module 1')
         module2 = Module.objects.create(name='Module 2')
@@ -36,7 +33,7 @@ class ContentModelTest(TestCase):
         self.assertIn(module2, path.modules.all(), "Path should contain 'Module 2'.")
 
     def test_module_lesson_relationship(self):
-        # Test that Lessons can be associated with a Module and retrieved correctly
+        # Test the relationship between modules and lessons
         module = Module.objects.create(name='Module 1')
         lesson1 = Lesson.objects.create(name='Lesson 1', lesson_type='text')
         lesson2 = Lesson.objects.create(name='Lesson 2', lesson_type='video')
@@ -47,7 +44,7 @@ class ContentModelTest(TestCase):
         self.assertIn(lesson2, module.lessons.all(), "Module should contain 'Lesson 2'.")
 
     def test_module_not_deleted_with_path(self):
-        # Test that deleting a Path does not delete its associated Modules
+        # Test that deleting a path does not delete its modules
         module = Module.objects.create(name='Module 1')
         path = Path.objects.create(name='Path 1')
         path.modules.add(module)
@@ -56,11 +53,11 @@ class ContentModelTest(TestCase):
         self.assertEqual(Module.objects.count(), 1, "Module should not be deleted when Path is deleted.")
 
     def test_lesson_not_deleted_with_module(self):
-        # Test that deleting a Module does not delete its associated Lessons
+        # Test that deleting a module does not delete its lessons
         module = Module.objects.create(name='Module 1')
         lesson = Lesson.objects.create(name='Lesson 1', lesson_type='text')
         module.lessons.add(lesson)
         module.delete()
 
         self.assertEqual(Lesson.objects.count(), 1, "Lesson should not be deleted when Module is deleted.")
-        self.assertFalse(module in lesson.modules.all(), "Lesson should no longer be linked to a deleted Module.")
+        self.assertNotIn(module, lesson.modules.all(), "Lesson should not be associated with any module after the module is deleted.")
