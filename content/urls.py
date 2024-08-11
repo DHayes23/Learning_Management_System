7ecc86bb4da136d9ee89538fdea64a022515812d
DHayes23 <D.Hayes23@hotmail.com>
2024-08-11 22:11:50 +0000

    Add paths to profile page and module detail page

--- content/urls.py
+++ content/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('path/<int:pk>/', views.path_detail, name='path_detail'),
+    path('module/<int:pk>/', views.module_detail, name='module_detail'),
 ]
