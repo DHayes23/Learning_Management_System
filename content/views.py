fd45acd606ad4288167d755119d274f62e1868c8
DHayes23 <D.Hayes23@hotmail.com>
2024-08-14 01:51:18 +0000

    Restyle lesson cards if completed

--- content/views.py
+++ content/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Path, Module
+from .models import Path, Module, StudentProgress
 
 def path_detail(request, pk):
     path = get_object_or_404(Path, pk=pk)
@@ -8,4 +8,10 @@ def path_detail(request, pk):
 def module_detail(request, pk):
     module = get_object_or_404(Module, pk=pk)
     lessons = module.lessons.all()
+    user_progress = StudentProgress.objects.filter(student=request.user)
+    
+    # Add a flag to each lesson if it's completed by the user
+    for lesson in lessons:
+        lesson.is_completed = user_progress.filter(lesson=lesson, completed=True).exists()
+
     return render(request, 'content/module_detail.html', {'module': module, 'lessons': lessons})
